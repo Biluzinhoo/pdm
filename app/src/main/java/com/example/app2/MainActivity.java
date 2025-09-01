ec5b538e8432926b1ec4c7c9e41462a9ef73adc1
Gustavo <gustatw07@gmail.com>
2025-09-01 09:04:23 -0300

    oii

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.app2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,42 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editTextMin, editTextMax;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       Log.d("ciclodevida","onCreate");
-    }
+        button = findViewById(R.id.button2);
+        editTextMax = findViewById(R.id.edMax);
+        editTextMin = findViewById(R.id.edMin);
+        tv = findViewById(R.id.textView2);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ciclodevida","onStart");
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ciclodevida","onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("ciclodevida","onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("ciclodevida","onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("ciclodevida","onDestroy");
+        button.setOnClickListener(v -> {
+            Random r = new Random();
+            int min,max;
+            min= Integer.parseInt(editTextMin.getText().toString());
+            max= Integer.parseInt(editTextMax.getText().toString());
+            int delta = max-min;
+            int sortiado = r.nextInt(delta)+min;
+            tv.setText(Integer.toString(sortiado));
+        });
     }
 }
