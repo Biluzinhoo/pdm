1926508a892243f5132f83ce2f43e0cbf58f56d6
Gustavo <gustatw07@gmail.com>
2025-10-13 09:09:16 -0300

    listagem planetas

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    PlanetaController planetaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listView = findViewById(R.id.listview);
+        planetaController = new PlanetaController();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, planetaController.getNomePlanetas());
+
+        listView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/app2/Planeta.java
@@ -0,0 +1,21 @@
+package com.example.app2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Planeta  {
+
+    String nome;
+    int foto;
+
+    public Planeta(String nome, int foto){
+        this.nome = nome;
+        this.foto = foto;
+    }
+
+}
