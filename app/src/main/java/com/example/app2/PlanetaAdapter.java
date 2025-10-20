466da292ba18a2563ce6f9a64a671c0b19ebc583
Gustavo <gustatw07@gmail.com>
2025-10-20 09:22:03 -0300

    planetas suados

--- app/src/main/java/com/example/app2/PlanetaAdapter.java
+++ app/src/main/java/com/example/app2/PlanetaAdapter.java
@@ -1,8 +1,38 @@
 package com.example.app2;
 
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class PlanetaAdapter extends ArrayAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import java.util.List;
 
+public class PlanetaAdapter extends ArrayAdapter<Planeta> {
+
+    int mResouce;
+    public PlanetaAdapter(@NonNull Context context, int resource, @NonNull List<Planeta> objects) {
+        super(context, resource, objects);
+        mResouce = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View v = layoutInflater.inflate(mResouce,parent,false);
+        Planeta planeta = getItem(position);
+        TextView tv = v.findViewById(R.id.textView);
+        ImageView iv = v.findViewById(R.id.imageView);
+        tv.setText(planeta.nome);
+        iv.setImageResource(planeta.foto);
+        return v;
+
+    }
 }
