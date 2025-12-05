e964b69768f0e2c6831b7fe77f4275d7bcac541d
Gustavo <gustatw07@gmail.com>
2025-12-05 10:30:44 -0300

    Arrumado o sqlite

--- app/src/main/java/com/example/app2/ExibeItem.java
+++ app/src/main/java/com/example/app2/ExibeItem.java
@@ -1,8 +1,13 @@
 package com.example.app2;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ExibeItem extends AppCompatActivity {
 
-    TextView textViewTitulo;
-    Button bntVoltar;
-
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,28 @@ public class ExibeItem extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-        bntVoltar = findViewById(R.id.fecharExibeCampo);
-        bntVoltar.setOnClickListener(v -> {finish();});
-        String titulo = getIntent().getStringExtra("titulo");
-        textViewTitulo = findViewById(R.id.textViewExibeItem);
-        textViewTitulo.setText(titulo);
+
+        Bundle bundle = getIntent().getExtras();
+        ContentValues cv = new ContentValues();
+
+        TextView titulo = findViewById(R.id.dado);
+        TextView texto = findViewById(R.id.campoTexto);
+        TextView id = findViewById(R.id.idDado);
+        Button save = findViewById(R.id.salvar);
+        Long idB = bundle.getLong("id");
+
+        String tituloA = bundle.getString("titulo", "Default String");
+        String textoA = bundle.getString("texto", "Default String");
+        titulo.setText(tituloA);
+        texto.setText(textoA);
+        id.setText(idB.toString());
+
+        db = openOrCreateDatabase("db", MODE_PRIVATE, null);
+        save.setOnClickListener(b -> {
+            cv.put("texto", texto.getText().toString());
+            db.update("notas", cv, "titulo = ?", new String[]{tituloA});
+            db.close();
+        });
     }
 }
--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.app2;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -20,79 +21,73 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     SQLiteDatabase db;
-    Button button;
-    EditText editText;
+
+    Button b;
+
     ListView listView;
 
-    EditText editText2;
+    @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        editText = findViewById(R.id.editTextText);
-        listView = findViewById(R.id.listView);
-        editText2 = findViewById(R.id.editTextText2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        db = openOrCreateDatabase("meu_banco.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS notas" +
-                "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, txt TEXT);");
+
+        listView = findViewById(R.id.listView2);
+        db = openOrCreateDatabase("db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, texto TEXT)");
+        ContentValues cv = new ContentValues();
+
+
+        b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            EditText editText = findViewById(R.id.name);
+            String texto = editText.getText().toString();
+            cv.put("texto", "Texto da nota");
+            cv.put("titulo", texto);
+            db.insert("notas",null, cv);
+            carregarListagem();
+            //Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
+        });
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Intent intent = new Intent(this, ExibeItem.class);
+
+            System.out.print(position);
+            Cursor cursor = db.rawQuery("SELECT * FROM notas where titulo = \""+parent.getItemAtPosition(position)+"\"", null);
+            cursor.moveToFirst();
+
+            Bundle bundle = new Bundle();
+            bundle.putString("titulo",cursor.getString(cursor.getColumnIndex("titulo" + "")));
+            bundle.putString("texto",cursor.getString(cursor.getColumnIndex("texto" + "")));
+
+            intent.putExtras(bundle);
+
+            startActivity(intent);
+        });
 
         carregarListagem();
-
-        button.setOnClickListener(v -> {
-            String titulo = editText.getText().toString();
-            ContentValues cv = new ContentValues();
-            cv.put("titulo", titulo);
-            db.insert("notas",null, cv);
-            carregarListagem();
-        });
-
-        button.setOnClickListener(v -> {
-            String texto = editText2.getText().toString();
-            ContentValues cv = new ContentValues();
-            cv.put("texto", texto);
-            db.insert("notas",null, cv);
-            carregarListagem();
-        });
-
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            String titulo = (String) parent.getItemAtPosition(position);
-            Intent intent = new Intent(MainActivity.this, ExibeItem.class);
-            intent.putExtra("titulo",titulo);
-            startActivity(intent);
-
-        });
-
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            String texto = (String) parent.getItemAtPosition(position);
-            Intent intent = new Intent(MainActivity.this, ExibeItem.class);
-            intent.putExtra("texto",texto);
-            startActivity(intent);
-
-        });
-
-
-
-
     }
 
     public void carregarListagem() {
-        ArrayList<String> titulos = new ArrayList<String>();
+        ArrayList<String> titulos = new ArrayList<>();
         Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
         cursor.moveToFirst();
 
         while(!cursor.isAfterLast()) {
-            String titulo = cursor.getString(cursor.getColumnIndex("titulo"));
+            @SuppressLint("Range") String titulo = cursor.getString(cursor.getColumnIndex("titulo" +
+                    ""));
             titulos.add(titulo);
             cursor.moveToNext();
+
         }
 
         ArrayAdapter<String> titulosAdapter = new ArrayAdapter<>(
@@ -102,6 +97,6 @@ public class MainActivity extends AppCompatActivity {
         );
 
         listView.setAdapter(titulosAdapter);
-    }
 
+    }
 }
